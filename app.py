680b1117adf9105b0d2367f3f268ac4ad68bb889
Christopher Vishnu <vishnuc@mcmaster.ca>
2022-01-13 00:15:14 -0500

    Send and receive messages with socketio

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 import socketio
 import eventlet
+import json
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
 
 @sio.event
-def connect(sid, environ):  ## session ID (), dictionary with details of client request 
+def connect(sid, environ):  ## session ID (), dictionary with details of client request
     print(sid, " connected")
     print(environ)
+
+@sio.event
+def my_message(sid, data):
+    print('message :', data)
+    print("sid: ", sid)
+    dic = { "we": "did", "it": "!"}
+    data = data.split()
+    jdata = json.dumps(data)
+    jdata2 = json.dumps(dic)
+    sio.emit("greetings", jdata2)
+
+@sio.event
+def Hello(sid, data):
+    print('message :', data)
 
 @sio.event
 def disconnect(sid):
